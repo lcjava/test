1d30ed9a334cbd4322af68770d4c34d52a9f2847
lcjava <lcjava@qq.com>
2015-05-15 14:47:18 +0800

    delete txt

--- code/2DSoftShadow/scene_2DSoftShadow.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  scene_2DSoftShadow.cpp
-//  HelloCpp
-//
-//  Created by yang chao (wantnon) on 14-6-17.
-//
-//
-
-#include "scene_2DSoftShadow.h"
--- code/break/scene_break.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  scene_break.cpp
-//  HelloCpp
-//
-//  Created by yang chao (wantnon) on 14-6-16.
-//
-//
-
-#include "scene_break.h"
